b7f961c60dbe4210d40f8512c4acd2b4438c3672
sanlouise <sandra.adamshallie@gmail.com>
2017-09-14 20:55:49 -0400

    Update SearchBar to handle error from API

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -11,20 +11,52 @@ class SearchBar extends Component {
     updateSearchTerm: PropTypes.func.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+    };
+  }
+
+  onChangeHandler = (event) => {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.props.updateSearchTerm(event);
+  }
+
   onSearch = () => {
-    getWeatherDetails(this.props.searchTerm, ({ city }) => {
+    getWeatherDetails(this.props.searchTerm, (res) => {
+      const statusCode = parseInt(res.cod, 10);
+      if (statusCode < 200 || statusCode >= 300) {
+        // Error fetching city data.
+        this.setState({ error: true });
+        return;
+      }
+
+      // Great, now let's normalize the data since we have it by
+      // putting our weather data INTO our city object.
+      const { city, list } = res;
+      city.lng = city.coord.lon;
+      city.lat = city.coord.lat;
+      city.list = list;
+      delete city.coord;
+
+      // Fantastic, let's add our city now.
       this.props.addCity(city);
     });
   }
 
   render() {
-    const { onSearch } = this;
-    const { searchTerm, updateSearchTerm } = this.props;
+    const { onChangeHandler, onSearch } = this;
+    const { searchTerm } = this.props;
+    const { error } = this.state;
 
     return (
       <div className="search-bar">
+        <p style={{ display: error ? 'inherit' : 'none' }}>Oops, something happened! Try again...</p>
         <input
-          onChange={updateSearchTerm}
+          onChange={onChangeHandler}
           onKeyDown={({ keyCode }) => keyCode === 13 ? onSearch() : undefined}
           placeholder="Search for a city in the USA..."
           type="text"
