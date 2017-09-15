a828c535d860fc99d28e68551171af03a86581a0
sanlouise <sandra.adamshallie@gmail.com>
2017-09-14 21:56:51 -0400

    Update env variables

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+const config = {
+  weather: {
+    KEY: process.env.REACT_APP_WEATHER_KEY || 'WEATHER_KEY',
+    URL: process.env.REACT_APP_WEATHER_URL || 'WEATHER_URL',
+  },
+};
+
+export default config;
