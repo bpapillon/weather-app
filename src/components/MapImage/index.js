96d9d3ccbac3bfea87ed7c7d2ac3618414839858
sanlouise <sandra.adamshallie@gmail.com>
2017-09-14 20:55:27 -0400

    Add Google Map component (MapImage)

---
+++ src/components/MapImage/index.js
@@ -0,0 +1,20 @@
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+import GoogleMapReact from 'google-map-react';
+// Internals
+import './styles.css';
+
+
+const MapImage = ({ lat, lng }) => (
+  <div className="google-map">
+    <GoogleMapReact defaultZoom={10} defaultCenter={{ lat, lng }} />
+  </div>
+);
+
+MapImage.propTypes = {
+  lat: PropTypes.number.isRequired,
+  lng: PropTypes.number.isRequired,
+}
+
+export default MapImage;
