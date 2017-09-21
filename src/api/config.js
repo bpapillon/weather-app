dd92b35f5b8129e9286315b5f16391edc398d7cf
sanlouise <sandra.adamshallie@gmail.com>
2017-09-21 15:53:25 -0400

    Update env vars for heroku

--- src/api/config.js
+++ src/api/config.js
@@ -3,6 +3,9 @@ const config = {
     KEY: process.env.REACT_APP_WEATHER_KEY || 'WEATHER_KEY',
     URL: process.env.REACT_APP_WEATHER_URL || 'WEATHER_URL',
   },
+  googleMaps: {
+    KEY: process.env.REACT_APP_GOOGLE_MAPS_KEY || 'GOOGLE_MAPS_KEY',
+  },
 };
 
 export default config;
--- src/components/MapImage/index.js
+++ src/components/MapImage/index.js
@@ -2,13 +2,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import GoogleMapReact from 'google-map-react';
+import get from 'lodash/get';
+// Externals
+import config from '../../api/config';
 // Internals
 import './styles.css';
+const key = get(config, 'googleMaps.KEY');
 
 
 const MapImage = ({ lat, lng }) => (
   <div className="google-map">
-    <GoogleMapReact key={process.env.GOOGLE_MAPS_KEY} defaultZoom={10} defaultCenter={{ lat, lng }} />
+    <GoogleMapReact
+      bootstrapURLKeys={{ key }}
+      defaultCenter={{ lat, lng }}
+      defaultZoom={10}
+    />
   </div>
 );
 
